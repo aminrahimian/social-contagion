8cb096a2c2c860a7a3ac5b58f2fd91bd81b9c0b9
Amin Rahimian <rahimian@mit.edu>
2018-06-20 15:35:35 -0400

    do the plots versus q_n not alphas

--- testing_speed_c_1_union_ER.py
+++ testing_speed_c_1_union_ER.py
@@ -14,14 +14,15 @@ size_of_dataset = 20
 
 if __name__ == '__main__':
 
-    alphas = [1 / 16, 2 / 16, 3 / 16, 4 / 16, 5 / 16, 6 / 16, 7/16, 8/16, 9/16,10/16]  # 7/8
+    alphas_logit = [2 / 17, 2 / 16, 3 / 16, 4 / 16, 5 / 16, 6 / 16, 7 / 16, 8 / 16, 9 / 16, 10 / 16]#, 8/16, 9/16,10/16]  # 7/8
 
     NET_SIZE = 1000
 
     speeds_1000_logit = []
     speeds_std_1000_logit = []
+    q_logit = []
 
-    for alpha in alphas:
+    for alpha in alphas_logit:
 
         params_1000_logit = {
             'zero_at_zero': False,
@@ -40,14 +41,21 @@ if __name__ == '__main__':
         speed, std, _, _ = dynamics.avg_speed_of_spread(dataset_size=size_of_dataset, mode='total')
         speeds_1000_logit.append(speed)
         speeds_std_1000_logit.append(std)
+        adoption_probabilities = dynamics.get_activation_probabilities()
+        q_logit.append(adoption_probabilities[1])
         print(speeds_1000_logit)
         print(speeds_std_1000_logit)
-        print(dynamics.get_activation_probabilities())
+        print(q_logit)
 
+################################################################################
+################################################################################
+
+    alphas_probit = [1 / 32, 2 / 32, 3 / 32, 4 / 32, 6 / 32, 8 / 32, 10 / 32, 12 / 32]
     speeds_1000_probit = []
     speeds_std_1000_probit = []
+    q_probit = []
 
-    for alpha in alphas:
+    for alpha in alphas_probit:
         params_1000_probit = {
             'zero_at_zero': False,
             'network_model': 'cycle_union_Erdos_Renyi',
@@ -65,14 +73,22 @@ if __name__ == '__main__':
         speed, std, _, _ = dynamics.avg_speed_of_spread(dataset_size=size_of_dataset, mode='total')
         speeds_1000_probit.append(speed)
         speeds_std_1000_probit.append(std)
+        adoption_probabilities = dynamics.get_activation_probabilities()
+        q_probit.append(adoption_probabilities[1])
         print(speeds_1000_probit)
         print(speeds_std_1000_probit)
-        print(dynamics.get_activation_probabilities())
+        print(q_probit)
 
+
+###################################################################################################
+###################################################################################################
+
+    alphas_mix = [3 / 16, 4 / 16, 5 / 16, 6 / 16, 7 / 16, 8 / 16, 9 / 16, 10 / 16]
     speeds_1000_mix = []
     speeds_std_1000_mix = []
+    q_mix = []
 
-    for alpha in alphas:
+    for alpha in alphas_mix:
         params_1000_mix = {
             'zero_at_zero': True,
             'network_model': 'cycle_union_Erdos_Renyi',
@@ -91,10 +107,11 @@ if __name__ == '__main__':
         speed, std, _, _ = dynamics.avg_speed_of_spread(dataset_size=size_of_dataset, mode='total')
         speeds_1000_mix.append(speed)
         speeds_std_1000_mix.append(std)
+        adoption_probabilities = dynamics.get_activation_probabilities()
+        q_mix.append(adoption_probabilities[1])
         print(speeds_1000_mix)
         print(speeds_std_1000_mix)
-
-        print(dynamics.get_activation_probabilities())
+        print(q_mix)
 
     # NET_SIZE = 3000
     #
@@ -147,18 +164,18 @@ if __name__ == '__main__':
     plt.figure(2)
     #
     if 'speeds_1000_logit' in vars() or 'speeds_1000_logit' in globals():
-        plt.errorbar(alphas, np.asarray(speeds_1000_logit), yerr=speeds_std_1000_logit, color='g', linewidth=1.5,
+        plt.errorbar(q_logit, np.asarray(speeds_1000_logit), yerr=speeds_std_1000_logit, color='g', linewidth=1.5,
                      label='Logit $\\sigma_n = 1/2\log(n^{\\alpha})$')
 
     if 'speeds_1000_probit' in vars() or 'speeds_1000_probit' in globals():
-        plt.errorbar(alphas, np.asarray(speeds_1000_probit), yerr=speeds_std_1000_probit, color='m', linewidth=1.5,
+        plt.errorbar(q_probit, np.asarray(speeds_1000_probit), yerr=speeds_std_1000_probit, color='m', linewidth=1.5,
                      label='Probit $\\sigma_n = 1/\sqrt{8\log(n^{\\alpha})}$')
 
-    plt.plot(alphas, [500]*len(alphas), color='c', linewidth=1,
+    plt.plot(q_mix, [500]*len(q_mix), color='c', linewidth=1,
                  label='The $\mathcal{C}_2$ benchmark')
 
     if 'speeds_1000_mix' in vars() or 'speeds_1000_mix' in globals():
-        plt.errorbar(alphas, np.asarray(speeds_1000_mix), yerr=speeds_std_1000_mix, color='r', linewidth=2.5,
+        plt.errorbar(q_mix, np.asarray(speeds_1000_mix), yerr=speeds_std_1000_mix, color='r', linewidth=1.5,
                      label='Linear Threshold $q_n = 1/n^{\\alpha}$')
 
     # plt.errorbar(alphas, speeds_3000, yerr=speeds_std_3000, color='g', linewidth=2.5,
@@ -168,9 +185,9 @@ if __name__ == '__main__':
     #              label='$n = 5000$')
 
     plt.ylabel('Time to Spread')
-    plt.xlabel('$\\alpha$')
-    plt.title('\centering Complex Contagion with Sub-threshold Adoptions '
-              '\\vspace{-10pt}  \\begin{center} over   $\mathcal{C}_{1} \\cup \mathcal{G}_{n,1/n}, n = 1000$ \\end{center}')
+    plt.xlabel('$q_n$')
+    plt.title('\centering Complex Contagion over $\mathcal{C}_{1} \\cup \mathcal{G}_{n,1/n},n = 1000$'
+              '\\vspace{-10pt}  \\begin{center}  with Sub-threshold Adoption Probabilities Parametrized by $\\alpha$ \\end{center}')
     plt.legend()
     # plt.show()
     plt.savefig('./data/' + 'speed_of_contagion_union_C_1.png')
