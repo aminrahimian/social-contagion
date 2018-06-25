9d1d977c1d52dc499680d61e1813523ae1b038df
Amin Rahimian <rahimian@mit.edu>
2018-06-25 11:59:56 -0400

    c=2 to get fixed expected degree 4

--- testing_speed_c_1_union_ER.py
+++ testing_speed_c_1_union_ER.py
@@ -14,7 +14,7 @@ size_of_dataset = 20
 
 if __name__ == '__main__':
 
-    alphas_logit = [2 / 17, 2 / 16, 3 / 16, 4 / 16, 5 / 16, 6 / 16, 7 / 16, 8 / 16, 9 / 16, 10 / 16]#, 8/16, 9/16,10/16]  # 7/8
+    alphas_logit = [2 / 17, 2 / 16, 3 / 16, 4 / 16, 5 / 16, 6 / 16, 7 / 16, 8 / 16, 9 / 16, 10 / 16, 11/16, 12/16]#, 8/16, 9/16,10/16]  # 7/8
 
     NET_SIZE = 1000
 
@@ -32,7 +32,7 @@ if __name__ == '__main__':
             'delta': 0.0000000000000001,  # recoveryProb,  # np.random.beta(5, 2, None), # recovery probability
             'nearest_neighbors': 2,
             'theta': 1.5,
-            'c': 1,
+            'c': 2,
             'sigma': 1/(2*np.log(NET_SIZE**alpha)),
         }
 
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 ################################################################################
 ################################################################################
 
-    alphas_probit = [1 / 32, 2 / 32, 3 / 32, 4 / 32, 6 / 32, 8 / 32, 10 / 32, 12 / 32]
+    alphas_probit = [1 / 32, 2 / 32, 3 / 32, 4 / 32, 6 / 32, 8 / 32, 10 / 32, 12 / 32, 14/32, 16/32]
     speeds_1000_probit = []
     speeds_std_1000_probit = []
     q_probit = []
@@ -64,7 +64,7 @@ if __name__ == '__main__':
             'delta': 0.0000000000000001,  # recoveryProb,  # np.random.beta(5, 2, None), # recovery probability
             'nearest_neighbors': 2,
             'theta': 1.5,
-            'c': 1,
+            'c': 2,
             'sigma': 1 / np.sqrt(8 * np.log(NET_SIZE ** alpha)),
         }
 
@@ -83,7 +83,7 @@ if __name__ == '__main__':
 ###################################################################################################
 ###################################################################################################
 
-    alphas_mix = [3 / 16, 4 / 16, 5 / 16, 6 / 16, 7 / 16, 8 / 16, 9 / 16, 10 / 16]
+    alphas_mix = [3 / 16, 4 / 16, 5 / 16, 6 / 16, 7 / 16, 8 / 16, 9 / 16, 10 / 16, 11/16, 12/16]
     speeds_1000_mix = []
     speeds_std_1000_mix = []
     q_mix = []
@@ -99,7 +99,7 @@ if __name__ == '__main__':
             'fixed_prob_high': 1.0,
             'fixed_prob': 1 / NET_SIZE ** alpha,
             'theta': 2,
-            'c': 1,
+            'c': 2,
         }
         print(params_1000_mix['size'])
 
@@ -113,81 +113,28 @@ if __name__ == '__main__':
         print(speeds_std_1000_mix)
         print(q_mix)
 
-    # NET_SIZE = 3000
-    #
-    # speeds_3000 = []
-    # speeds_std_3000 = []
-    #
-    # for alpha in alphas:
-    #     params_3000 = {
-    #         'network_model': 'cycle_union_Erdos_Renyi',
-    #         'size': NET_SIZE,  # populationSize,
-    #         'initial_states': [1] + [1] + [0] * (NET_SIZE - 2),  # two initial seeds, next to each other
-    #         'delta': 0.0000000000000001,  # recoveryProb,  # np.random.beta(5, 2, None), # recovery probability
-    #         'nearest_neighbors': 2,
-    #         'theta': 2,
-    #         'c': 1,
-    #         'sigma': 1 / np.log(NET_SIZE**alpha),
-    #     }
-    #     print(params_3000['size'])
-    #     dynamics = logit(params_3000)
-    #     speed, std, _, _ = dynamics.avg_speed_of_spread(dataset_size=size_of_dataset, mode='total')
-    #     speeds_3000.append(speed)
-    #     speeds_std_3000.append(std)
-    #     print(speeds_3000)
-    #     print(speeds_std_3000)
-    #
-    # NET_SIZE = 5000
-    #
-    # speeds_5000 = []
-    # speeds_std_5000 = []
-    #
-    # for alpha in alphas:
-    #     params_5000 = {
-    #         'network_model': 'cycle_union_Erdos_Renyi',
-    #         'size': NET_SIZE,  # populationSize,
-    #         'initial_states': [1] + [1] + [0] * (NET_SIZE - 2),  # two initial seeds, next to each other
-    #         'delta': 0.0000000000000001,  # recoveryProb,  # np.random.beta(5, 2, None), # recovery probability
-    #         'nearest_neighbors': 2,
-    #         'theta': 2,
-    #         'c': 1,
-    #         'sigma': 1 / np.log(NET_SIZE**alpha),
-    #     }
-    #     print(params_5000['size'])
-    #     dynamics = logit(params_5000)
-    #     speed, std, _, _ = dynamics.avg_speed_of_spread(dataset_size=size_of_dataset, mode='total')
-    #     speeds_5000.append(speed)
-    #     speeds_std_5000.append(std)
-    #     print(speeds_5000)
-    #     print(speeds_std_5000)
-
     plt.figure(2)
     #
     if 'speeds_1000_logit' in vars() or 'speeds_1000_logit' in globals():
         plt.errorbar(q_logit, np.asarray(speeds_1000_logit), yerr=speeds_std_1000_logit, color='g', linewidth=1.5,
-                     label='Logit $\\sigma_n = 1/2\log(n^{\\alpha})$')
+                     label='Logit') # $\\sigma_n = 1/2\log(n^{\\alpha})$
 
     if 'speeds_1000_probit' in vars() or 'speeds_1000_probit' in globals():
         plt.errorbar(q_probit, np.asarray(speeds_1000_probit), yerr=speeds_std_1000_probit, color='m', linewidth=1.5,
-                     label='Probit $\\sigma_n = 1/\sqrt{8\log(n^{\\alpha})}$')
+                     label='Probit') #  $\\sigma_n = 1/\sqrt{8\log(n^{\\alpha})}$
 
     plt.plot(q_mix, [500]*len(q_mix), color='c', linewidth=1,
-                 label='The $\mathcal{C}_2$ benchmark')
+                 label='$\mathcal{C}_2$ benchmark')
 
     if 'speeds_1000_mix' in vars() or 'speeds_1000_mix' in globals():
         plt.errorbar(q_mix, np.asarray(speeds_1000_mix), yerr=speeds_std_1000_mix, color='r', linewidth=1.5,
-                     label='Linear Threshold $q_n = 1/n^{\\alpha}$')
+                     label='Threshold') #  $q_n = 1/n^{\\alpha}$
 
-    # plt.errorbar(alphas, speeds_3000, yerr=speeds_std_3000, color='g', linewidth=2.5,
-    #              label='$n = 3000$')
-    #
-    # plt.errorbar(alphas, speeds_5000, yerr=speeds_std_5000, color='c', linewidth=2.5,
-    #              label='$n = 5000$')
 
     plt.ylabel('Time to Spread')
-    plt.xlabel('$q_n$')
-    plt.title('\centering Complex Contagion over $\mathcal{C}_{1} \\cup \mathcal{G}_{n,1/n},n = 1000$'
-              '\\vspace{-10pt}  \\begin{center}  with Sub-threshold Adoption Probabilities Parametrized by $\\alpha$ \\end{center}')
+    plt.xlabel('Probability of Adoptions below Threshold $(q_n)$')
+    plt.title('\centering Complex Contagion over $\mathcal{C}_{1} \\cup \mathcal{G}_{n,2/n},n = 1000$'
+              '\\vspace{-10pt}  \\begin{center}  with Sub-threshold Adoptions \\end{center}')
     plt.legend()
     # plt.show()
     plt.savefig('./data/' + 'speed_of_contagion_union_C_1.png')
