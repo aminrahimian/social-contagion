d41c1fd3c2888fd3c15ee12c568efea54dc68f29
Amin Rahimian <rahimian@mit.edu>
2018-10-01 18:14:30 -0400

    simulations for theorem 1 spread time of ck union ER with multi processing replaces testing_speed_cycle_union_ER.py

--- settings.py
+++ settings.py
@@ -183,7 +183,8 @@ else:
 
 # theory simulations settings:
 
-simulation_type = 'c1_c2_interpolation_SimpleOnlyAlongC1'
+simulation_type = 'ck_union_ER_vs_k'#'ck_union_ER_vs_size' # 'ck_union_ER_vs_k'
+#'c1_c2_interpolation_SimpleOnlyAlongC1'
 #'c1_union_ER'
 # 'c1_c2_interpolation_SimpleOnlyAlongC1'
 
@@ -211,17 +212,44 @@ except OSError as e:
     if e.errno != errno.EEXIST:
         raise
 
-# general settings:
+# commonly used settings:
+
+# for computations:
+#
+# do_computations = True
+# do_multiprocessing = True
+# save_computations = True
+# load_computations = False
+# do_plots = False
+# save_plots = False
+# show_plots = False
+# data_dump = False
+# simulator_mode = False
+
+# for plotting:
 
 do_computations = False
 do_multiprocessing = False
 save_computations = False
 load_computations = True
-do_plots = False
-save_plots = False
+do_plots = True
+save_plots = True
 show_plots = False
 data_dump = False
-simulator_mode = True
+simulator_mode = False
+
+# for data_dump:
+#
+# do_computations = False
+# do_multiprocessing = False
+# save_computations = False
+# load_computations = True
+# do_plots = False
+# save_plots = False
+# show_plots = False
+# data_dump = True
+# simulator_mode = False
+
 
 
 #  check that different modes are set consistently
