d06e9262648b7dc06204c17af8f445e9839683ad
Amin Rahimian <rahimian@mit.edu>
2018-10-03 21:27:29 -0400

    collecting new samples for q_new_new

--- plot_spread_time_c1_c2_interpolation.py
+++ plot_spread_time_c1_c2_interpolation.py
@@ -4,6 +4,7 @@
 from models import *
 
 from computing_spread_time_c1_c2_interpolation import etas, all_q_labels, all_qs, size_of_dataset, \
+    q_labels_old,q_labels, \
     network_size
 
 if __name__ == '__main__':
@@ -26,14 +27,15 @@ if __name__ == '__main__':
     plt.figure()
 
     for q_label in all_q_labels:
+        if q_label not in q_labels_old+q_labels:
 
-        print(etas)
-        print(avg_spread_times[all_q_labels.index(q_label)])
+            print(etas)
+            print(avg_spread_times[all_q_labels.index(q_label)])
 
-        plt.errorbar(etas, avg_spread_times[all_q_labels.index(q_label)],
-                     yerr=[1.96 * s / np.sqrt(size_of_dataset) for s in std_spread_times[all_q_labels.index(q_label)]],
-                     linewidth=2.5, label='$q = '+str(Decimal(all_qs[all_q_labels.index(q_label)]).quantize(FOURPLACES))
-                                          +'$')
+            plt.errorbar(etas, avg_spread_times[all_q_labels.index(q_label)],
+                         yerr=[1.96 * s / np.sqrt(size_of_dataset) for s in std_spread_times[all_q_labels.index(q_label)]],
+                         linewidth=2.5, label='$q = '+str(Decimal(all_qs[all_q_labels.index(q_label)]).quantize(FOURPLACES))
+                                              +'$')
 
     plt.ylabel('time to spread')
     plt.xlabel('edges rewired ($\\eta$)')
