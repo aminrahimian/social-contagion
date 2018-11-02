8c6bb4d2369d69c8cf0e598e631793012cfbd95c
Amin Rahimian <rahimian@mit.edu>
2018-11-02 09:24:06 -0400

    fixed the normalization to n^(1/2) for c_k union random graph versus size plots

--- settings.py
+++ settings.py
@@ -184,7 +184,8 @@ else:
 
 # theory simulations settings:
 
-simulation_type = 'c1_c2_interpolation'
+simulation_type = 'ck_union_ER_vs_size'
+#  'c1_c2_interpolation'
 #  'ck_union_ER_vs_size'
 #  'ck_union_ER_vs_k'
 #  'c1_c2_interpolation'
@@ -218,26 +219,26 @@ except OSError as e:
 # commonly used settings:
 
 # for computations:
-do_computations = True
-do_multiprocessing = True
-save_computations = True
-load_computations = False
-do_plots = False
-save_plots = False
-show_plots = False
-data_dump = False
-simulator_mode = False
-#
-# # for plotting:
-# do_computations = False
-# do_multiprocessing = False
-# save_computations = False
-# load_computations = True
-# do_plots = True
-# save_plots = True
+# do_computations = True
+# do_multiprocessing = True
+# save_computations = True
+# load_computations = False
+# do_plots = False
+# save_plots = False
 # show_plots = False
 # data_dump = False
 # simulator_mode = False
+#
+# for plotting:
+do_computations = False
+do_multiprocessing = False
+save_computations = False
+load_computations = True
+do_plots = True
+save_plots = True
+show_plots = False
+data_dump = False
+simulator_mode = False
 
 # for data_dump:
 # do_computations = False
