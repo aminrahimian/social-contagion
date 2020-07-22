aee0919e1fbde9c3d2e2c81fc3e0d6d57020b10f
Amin Rahimian <rahimian@mit.edu>
2020-07-22 12:05:21 -0400

    plotting figure 3B - delta

--- plot_spread_time_c1_union_ER_w_delta.py
+++ plot_spread_time_c1_union_ER_w_delta.py
@@ -3,9 +3,12 @@
 
 from models import *
 
-from computing_spread_time_c1_union_ER_w_delta import deltas, delta_labels, qs, size_of_dataset, q_labels
+from computing_spread_time_c1_union_ER_w_delta import deltas, qs, size_of_dataset, q_labels
 
 if __name__ == '__main__':
+
+    CUT = len([0.0, 0.0020408163265306124, 0.0040816326530612249, 0.0061224489795918373,
+               0.0081632653061224497, 0.010204081632653062])
 
     assert do_plots and load_computations, "we should be in load_computations and do_plots mode!"
 
@@ -21,31 +24,37 @@ if __name__ == '__main__':
                                         + simulation_type
                                         + '.pkl', 'rb'))
 
-    plt.figure()
+    plt.figure(1,(9.2, 8))
 
     # overlay the plots for each q
     for q_label in q_labels:
         print(q_label)
-        print(deltas)
-        print(avg_spread_times[q_labels.index(q_label)])
+        print(deltas[:CUT])
+        print(avg_spread_times[q_labels.index(q_label)][:CUT])
 
-        plt.errorbar(deltas, avg_spread_times[q_labels.index(q_label)],
+        plt.errorbar(deltas[:CUT], avg_spread_times[q_labels.index(q_label)][:CUT],
                      yerr=[1.96 * s / np.sqrt(size_of_dataset)
-                           for s in std_spread_times[q_labels.index(q_label)]],
+                           for s in std_spread_times[q_labels.index(q_label)][:CUT]],
                      linewidth=2.5, label='$q = '
                                           + str(Decimal(qs[q_labels.index(q_label)]).quantize(FOURPLACES))
                                           + '$')
 
-    plt.ylabel('time to spread')
-    plt.xlabel('edges rewired ($\\eta$)')
+    plt.plot(deltas[:CUT],
+             [250] * CUT,
+             color='c', linewidth=1,linestyle='dashed')
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.ylabel('time to spread', fontsize=20)
+    plt.xlabel('reversion probability ($\\delta$)', fontsize=20)
     # plt.title('\centering Complex Contagion over $\mathcal{C}_2^{\eta}, n = ' + str(network_size) + '$'
     #           '\\vspace{-10pt}  \\begin{center}  with Sub-threshold Adoptions $(q)$ '
     #           'and Rewiring $(\eta)$   \\end{center}')
 
-    plt.legend(loc='upper left', fontsize=13)#bbox_to_anchor=(1, 0.065),
+    plt.legend(loc='upper left', fontsize=20)#bbox_to_anchor=(1, 0.065),
     # elif simulation_type == 'c1_c2_interpolation':
     #     plt.legend(loc='lower right', bbox_to_anchor=(1, 0.065), fontsize=11)  #
     if show_plots:
         plt.show()
     if save_plots:
         plt.savefig(theory_simulation_output_address + simulation_type + '.pdf')
+        plt.savefig('./figures/' + 'figure3B.pdf')
