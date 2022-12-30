591dc90fbda06dcf82050257c856110224aaafbe
Amin Rahimian <rahimian.amin@gmail.com>
2022-12-30 04:59:47 -0500

    showing fewer k values in the vs network size plot
    file to plot spread time of complex contagion over ck union ER random graphs   # versus k and network size

--- plot_spread_time_c_k_union_ER.R
+++ plot_spread_time_c_k_union_ER.R
@@ -1,3 +1,7 @@
+# file to plot figure S4 in https://arxiv.org/abs/1810.03579v4
+# spread time of complex contagion over ck union ER random graphs  
+# versus k and network size 
+
 library(latex2exp)
 library(dplyr)
 library(ggplot2)
@@ -19,7 +23,7 @@ ck_union_ER_vs_kspreading_data <- ck_union_ER_vs_kspreading_data %>%
     time_to_spread_ub = time_to_spread + std_in_spread
   )
 
-# ploting
+# plotting
 
 theme_set(theme_bw())
 theme_update(
@@ -28,19 +32,19 @@ theme_update(
 )
 
 size_colors <- c(
-  "250" = brewer.pal(8, "Set1")[1],
-  "1000" = brewer.pal(8, "Set1")[2],
-  "3000" = brewer.pal(8, "Set1")[3],
-  "5000" = brewer.pal(8, "Set1")[4],
-  "7000" = brewer.pal(8, "Set1")[5]
+  #"250" = brewer.pal(8, "Set1")[1],
+  "1000" = brewer.pal(8, "Set1")[1],#[2],
+  "3000" = brewer.pal(8, "Set1")[2],#[3],
+  "5000" = brewer.pal(8, "Set1")[5]#[4]#,
+  #"7000" = brewer.pal(8, "Set1")[5]
 )
 
 size_shapes <- c(
-  "250" = 20,
+  #"250" = 20,
   "1000" = 21,
   "3000" = 22,
-  "5000" = 23,
-  "7000" = 24
+  "5000" = 23#,
+  #"7000" = 24
 )
 
 # Use 95% confidence interval instead of SEM
@@ -51,7 +55,7 @@ ck_union_ER_vs_k_plot_line_error_bar <- ggplot(
       color=factor(network_size),
       shape=factor(network_size),
       fill=factor(network_size)),
-  data = ck_union_ER_vs_kspreading_data) +
+  data = ck_union_ER_vs_kspreading_data%>%filter(network_size %in% c(1000,3000,5000))) +
   scale_color_manual(name = "network size",values = size_colors) + 
   scale_fill_manual(name = "network size",values = size_colors) +
   scale_shape_manual(name = "network size",values = size_shapes) +
@@ -61,9 +65,9 @@ ck_union_ER_vs_k_plot_line_error_bar <- ggplot(
   geom_point(size=2) +
   theme(
     legend.justification=c(1, 1),
-    legend.position=c(0.4, 0.98),
+    legend.position=c(0.3, 0.98),
     legend.key = element_rect(size = 2),
-    legend.key.size = unit(1, 'lines')
+    legend.key.size = unit(2, 'lines')
   ) + xlab("order of cycle-power (k)") + ylab("mean time to spread")
 
 ck_union_ER_vs_k_plot_line_error_bar
@@ -73,8 +77,8 @@ ggsave(paste(cwd,'/data/theory-simulations/ck_union_ER_vs_k/output/ck_union_ER_v
        width = 5, height = 4)
 
 k_colors <- c(
-  "5" = brewer.pal(8, "Set1")[3],
-  "6" = brewer.pal(8, "Set1")[4],
+  "5" = brewer.pal(8, "Set1")[1],
+  "6" = brewer.pal(8, "Set1")[2],
   "7" = brewer.pal(8, "Set1")[5]
 )
 
