302a3862eaf164c4f1bedb64f70d7585f7e3f586
Albertlong039 <92559960+Albertlong039@users.noreply.github.com>
2023-02-11 15:25:14 -0500

    Update plot_spread_time_c_1_union_ER.R
    add codes to plot spread time vs network size under different q

--- plot_spread_time_c_1_union_ER.R
+++ plot_spread_time_c_1_union_ER.R
@@ -7,13 +7,13 @@ library(plotrix)
 
 cwd <- dirname(rstudioapi::getSourceEditorContext()$path)
 
-c1_union_ER_vs_q_data <- read.csv(
+c1_union_ER_vs_network_size_data <- read.csv(
   paste(cwd,"/data/theory-simulations/c1_union_ER/output/c1_union_ERspreading_data_dump.csv",sep=""),
   stringsAsFactors = FALSE
 )
 
 
-c1_union_ER_vs_q_data <- c1_union_ER_vs_q_data %>%
+c1_union_ER_vs_network_size_data <- c1_union_ER_vs_network_size_data %>%
   mutate(
     time_to_spread_lb = time_to_spread - std_in_spread,
     time_to_spread_ub = time_to_spread + std_in_spread
@@ -40,14 +40,14 @@ size_shapes <- c(
 )
 
 # Use 95% confidence interval instead of SEM
-c1_union_ER_vs_q_plot_line_error_bar <- ggplot(
+c1_union_ER_vs_network_size_plot_line_error_bar <- ggplot(
   aes(x =network_size, 
       y=time_to_spread,
       group=factor(q),
       color=factor(q),
       shape=factor(q),
       fill=factor(q)),
-  data = c1_union_ER_vs_q_data) +
+  data = c1_union_ER_vs_network_size_data) +
   scale_color_manual(name = "q",values = size_colors) + 
   scale_fill_manual(name = "q",values = size_colors) +
   scale_shape_manual(name = "q",values = size_shapes) +
@@ -62,9 +62,9 @@ c1_union_ER_vs_q_plot_line_error_bar <- ggplot(
     legend.key.size = unit(1, 'lines')
   ) + xlab("network size") + ylab("mean time to spread")
 
-c1_union_ER_vs_q_plot_line_error_bar
+c1_union_ER_vs_network_size_plot_line_error_bar
 
 ggsave(paste(cwd,"/data/theory-simulations/c1_union_ER/output/c1_union_ER_vs_q_plot_line_error_bar.pdf",sep=""),
-       c1_union_ER_vs_q_plot_line_error_bar,
+       c1_union_ER_vs_network_size_plot_line_error_bar,
        width = 5, height = 4)
 
