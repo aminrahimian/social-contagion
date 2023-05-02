fb81488241c5a01cd5351ab8649a9fd85af26693
Albertlong039 <92559960+Albertlong039@users.noreply.github.com>
2023-05-02 16:45:50 -0400

    add new codes for Z2_union_ER plotting

--- computing_spread_time_c_1_union_ER_vs_network_size.py
+++ computing_spread_time_c_1_union_ER_vs_network_size.py
@@ -1,5 +1,4 @@
 # Spread time in C_1 uninon G(n,p_n) allowing for simple adoptions with probability (q) - theorem 2 simulations.
-
 from models import *
 
 size_of_dataset = 500
@@ -8,15 +7,19 @@ model = 'Threshold'
 
 fixed_prob_low = [0.025, 0.05, 0.075]
 network_size = [250, 500, 750, 1000, 1250]
+# # if for lattice calculation
+# fixed_prob_low = [0.01, 0.02, 0.03]
+# NETWORK_SIZE = [100, 400, 900, 1600, 2500]
+
 
 def compute_spread_time(NET_size, Q):
-
     if model == 'Threshold':
         params_mix = {
             'zero_at_zero': True,
-            'network_model': 'cycle_union_Erdos_Renyi',
+            'network_model': 'cycle_union_Erdos_Renyi',  # 'two_d_lattice_union_Erdos_Renyi'
             'size': NET_size,  # populationSize,
-            'initial_states': [infected*active] + [infected*active] + [susceptible] * (NET_size - 2),
+            # 'initialization_mode': 'fixed_number_initial_infection_at_center',
+            'initial_states': [infected * active] + [infected * active] + [susceptible] * (NET_size - 2),
             # two initial seeds, next to each other
             'delta': 0.0,  # recoveryProb,  # np.random.beta(5, 2, None), # recovery probability
             'nearest_neighbors': 2,
@@ -40,28 +43,28 @@ def compute_spread_time(NET_size, Q):
     q = adoption_probabilities[1]
 
     if save_computations:
-        for size in network_size:
-            address = theory_simulation_pickle_address + str(size) + '/'
-            pickle.dump(spread_time_std, open(address
-                                              + model + '_Q_'
-                                              + str(fixed_prob_low.index(Q))
-                                              + '_spread_time_std.pkl', 'wb'))
-            pickle.dump(spread_time_avg, open(address
-                                              + model + '_Q_'
-                                              + str(fixed_prob_low.index(Q))
-                                              + '_spread_time_avg.pkl', 'wb'))
-            pickle.dump(q, open(address
-                                + model + '_Q_'
-                                + str(fixed_prob_low.index(Q))
-                                + '_q.pkl', 'wb'))
+        address = theory_simulation_pickle_address + str(NET_size) + '/'
+        pickle.dump(spread_time_std, open(address
+                                          + model + '_Q_'
+                                          + str(fixed_prob_low.index(Q))
+                                          + '_spread_time_std.pkl', 'wb'))
+        pickle.dump(spread_time_avg, open(address
+                                          + model + '_Q_'
+                                          + str(fixed_prob_low.index(Q))
+                                          + '_spread_time_avg.pkl', 'wb'))
+        pickle.dump(q, open(address
+                            + model + '_Q_'
+                            + str(fixed_prob_low.index(Q))
+                            + '_q.pkl', 'wb'))
 
 
 if __name__ == '__main__':
 
     assert do_computations or data_dump, "we should be in do_computations or data_dump mode!"
     assert simulation_type == 'c1_union_ER', "simulation type is: " + simulation_type
+    # assert simulation_type == 'two_d_lattice_union_Erdos_Renyi', "simulation type is: " + simulation_type
 
-    input_list = [(net_size, Q)for net_size in network_size for Q in fixed_prob_low]
+    input_list = [(net_size, Q) for net_size in NETWORK_SIZE for Q in fixed_prob_low]
 
     if not data_dump:
         if do_multiprocessing:
@@ -70,20 +73,23 @@ if __name__ == '__main__':
             pool.close()
             pool.join()
         else:
-            for net_size in network_size:
+            for net_size in NETWORK_SIZE:
                 for Q in fixed_prob_low:
                     compute_spread_time(model, Q, net_size)
     elif data_dump:
         # load individual avg and std pkl files, organize them in a list and save them in a pair of pkl files
         # one for all the avg's and the other for all the std's.
-        qs = []
-        spread_time_avgs = []
-        spread_time_stds = []
+
         df = pd.DataFrame(columns=['network_size', 'time_to_spread', 'std_in_spread', 'q'])
 
-        for size in network_size:
+        for size in NETWORK_SIZE:
             address = theory_simulation_pickle_address + str(size) + '/'
+            qs = []
+            spread_time_avgs = []
+            spread_time_stds = []
             for Q in fixed_prob_low:
+                index = fixed_prob_low.index(Q)
+
                 spread_time_avg = pickle.load(open(address
                                                    + model + '_Q_'
                                                    + str(fixed_prob_low.index(Q))
@@ -93,6 +99,7 @@ if __name__ == '__main__':
                                                    + model + '_Q_'
                                                    + str(fixed_prob_low.index(Q))
                                                    + '_spread_time_std.pkl', 'rb'))
+
                 q = pickle.load(open(address
                                      + model + '_Q_'
                                      + str(fixed_prob_low.index(Q))
@@ -103,14 +110,14 @@ if __name__ == '__main__':
                 spread_time_avgs.append(spread_time_avg)
 
                 spread_time_stds.append(spread_time_avg)
+
             for Q in fixed_prob_low:
                 index = fixed_prob_low.index(Q)
                 temp = {'network_size': size,
                         'time_to_spread': spread_time_avgs[index],
-                        'std_in_spread': 1.96 * spread_time_stds[index] / np.sqrt(size),
+                        'std_in_spread': 1.96 * spread_time_stds[index] / np.sqrt(size_of_dataset),
                         'q': qs[index]}
                 df = df.append(temp, ignore_index=True)
         pd.DataFrame(df).to_csv(theory_simulation_output_address
                                 + simulation_type + 'spreading_data_dump.csv',
                                 index=False)
-
--- plot_spread_time_c_1_union_ER.py
+++ plot_spread_time_c_1_union_ER.py
@@ -57,3 +57,85 @@ if __name__ == '__main__':
     if save_plots:
         # plt.savefig(theory_simulation_output_address + simulation_type + '.png')
         plt.savefig('./figures/' + 'figureS6B.pdf')
+
+
+# # plotting the computed spread times in computing_spread_time_Z2_union_ER
+#
+# from settings import *
+# from computing_spread_time_c_1_union_ER import models_list, size_of_dataset, NET_SIZE
+#
+# models_list = ['noisy 2-complex'] # renaming labels
+# models_list1 = ['Z4 benchmark']
+#
+# if __name__ == '__main__':
+#
+#     assert do_plots and load_computations, "we should be in load_computations and do_plots mode!"
+#
+#     assert simulation_type == 'Z2_union_ER', \
+#         "we are not in the right simulation_type:" + simulation_type
+#
+#     spread_time_stds = pickle.load(open(theory_simulation_pickle_address
+#                                         + 'Z2_union_ER/'
+#                                         + simulation_type
+#                                         + '_spread_times_std.pkl', 'rb'))
+#
+#     spread_time_avgs = pickle.load(open(theory_simulation_pickle_address
+#                                         + 'Z2_union_ER/'
+#                                         + simulation_type
+#                                         + '_spread_time_avgs.pkl', 'rb'))
+#
+#     qs = pickle.load(open(theory_simulation_pickle_address
+#                           + 'Z2_union_ER/'
+#                           + simulation_type
+#                           + '_qs.pkl', 'rb'))
+#
+#     plt.figure(1,(7, 6))
+#     spread_time_avgs_benchmark = pickle.load(open(theory_simulation_pickle_address
+#                                         + 'Z4/'
+#                                         + simulation_type
+#                                         + '_spread_time_avgs.pkl', 'rb'))
+#     spread_time_stds_benchmark = pickle.load(open(theory_simulation_pickle_address
+#                                         + 'Z4/'
+#                                         + simulation_type
+#                                         + '_spread_times_std.pkl', 'rb'))
+#     qs_benchmark = pickle.load(open(theory_simulation_pickle_address
+#                           + 'Z4/'
+#                           + simulation_type
+#                           + '_qs.pkl', 'rb'))
+#
+#     for model in models_list:
+#         model_index = models_list.index(model)
+#         plt.errorbar(qs[model_index],
+#                      spread_time_avgs[model_index],
+#                      yerr=[1.96*s/np.sqrt(size_of_dataset) for s in spread_time_stds[model_index]],
+#                      linewidth=1.5,
+#                      label=model)
+#
+#     for model in models_list1:
+#         model_index = models_list1.index(model)
+#         plt.errorbar(qs_benchmark[model_index],
+#                      spread_time_avgs_benchmark[model_index],
+#                      yerr=[1.96 * s / np.sqrt(size_of_dataset) for s in spread_time_stds_benchmark[model_index]],
+#                      linewidth=1.5,
+#                      color='r', linestyle='--',
+#                      label = '$\mathcal{Z}_4$ benchmark')
+#
+#
+#     plt.xscale("log")
+#     plt.ylabel('time to spread', fontsize=20)
+#     plt.xlabel('probability of adoptions below threshold $(q)$', fontsize=20)
+#     # plt.title('\centering Complex Contagion over $\mathcal{C}_{1} \\cup \mathcal{G}_{n,2/n},n = '
+#     #           + str(NET_SIZE) + '$'
+#     #           '\\vspace{-10pt}  \\begin{center}  with Sub-threshold Adoptions \\end{center}', fontsize=18)
+#     plt.legend(loc='upper right', prop={'size': 20})
+#     plt.arrow(0.01105, 90, 0, -90, width=0.0001, head_width=0.001,
+#               head_length=15, capstyle='projecting',
+#               length_includes_head=True, shape='full', joinstyle='round', fc='k', ec='k')
+#     plt.text(0.012, 30, '$0.01105$',
+#              fontweight='bold', fontdict=None, withdash=False)
+#     if show_plots:
+#         plt.show()
+#     if save_plots:
+#         # plt.savefig(theory_simulation_output_address + simulation_type + '.png')
+#         plt.savefig('./figures/' + 'Z2_union_ER_vs_q.pdf')
+
